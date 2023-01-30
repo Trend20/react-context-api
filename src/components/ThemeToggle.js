85a519450435256de3771a5d4f7a78cc8361eeae
Trend20 <enockomondi305@gmail.com>
2023-01-30 12:31:33 +0300

    context on functional component

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../context/ThemeContext';
 
-class ThemeToggle extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const {toggleTheme} = this.context;
-    return (
-      <>
-        <button onClick={toggleTheme}>Toggle Theme</button>
-      </>
-    )
-  }
+
+// CLASS COMPONENT
+// class ThemeToggle extends Component {
+//   static contextType = ThemeContext;
+//   render() {
+//     const {toggleTheme} = this.context;
+//     return (
+//       <>
+//         <button onClick={toggleTheme}>Toggle Theme</button>
+//       </>
+//     )
+//   }
+// }
+
+// FUNCTION COMPONENT
+const ThemeToggle = () =>{
+  const {toggleTheme} = useContext(ThemeContext);
+  return (
+    <>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </>
+  )
 }
 
 export default ThemeToggle;
