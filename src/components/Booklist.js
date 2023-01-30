d191dabecef9d4e9c6e8ead797b205b5b86a0cda
Trend20 <enockomondi305@gmail.com>
2023-01-30 12:18:42 +0300

    context on functional component

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
-import { ThemeContext } from '../context/ThemeContext';
+import  { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
-class Booklist extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { light, dark, isLightTheme} = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div className='book-list' style={{ background: theme.bg, color: theme.syntax }}>
-        <ul>
-          <li style={{ background: theme.ui }}>the way to the king</li>
-          <li style={{ background: theme.ui }}>time management</li>
-          <li style={{ background: theme.ui }}>master the art</li>
-        </ul>
-      </div>
-    );
-  }
-}
+// CLASS COMPONENT
+// class Booklist extends Component {
+//   static contextType = ThemeContext;
+//   render() {
+//     const { light, dark, isLightTheme} = this.context;
+//     const theme = isLightTheme ? light : dark;
+//     return (
+//       <div className='book-list' style={{ background: theme.bg, color: theme.syntax }}>
+//         <ul>
+//           <li style={{ background: theme.ui }}>the way to the king</li>
+//           <li style={{ background: theme.ui }}>time management</li>
+//           <li style={{ background: theme.ui }}>master the art</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+// FUNCTIONAL COMPONENT
+const Booklist = () => {
+  const {light, dark, isLightTheme }= useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className="book-list"
+      style={{ background: theme.bg, color: theme.syntax }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>the way to the king</li>
+        <li style={{ background: theme.ui }}>time management</li>
+        <li style={{ background: theme.ui }}>master the art</li>
+      </ul>
+    </div>
+  );
+};
 
 export default Booklist;
